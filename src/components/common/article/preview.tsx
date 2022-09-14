b1fb2f2aae63868d77b3913fc92a4f3a2264bb1c
유병재 <ybj1217@midasin.com>
2022-09-14 15:43:07 +0900

    blog-preview table

---
+++ src/components/common/article/preview.tsx
@@ -0,0 +1,49 @@
+import { EColor } from "@defines/css";
+import { BlogMainProps } from "@pages/blog/main/[name]"
+import classNames from "classnames";
+
+export function PreviewTable(props: BlogMainProps) {
+    const {
+        articles
+    } = props;
+    console.log(articles);
+    return (
+        <>
+            <div className={classNames('preview-table-wrapper')}>
+                {
+                    articles.map(({title, description, uploadDate, thumbnail}) => {
+                        return (
+                            // key 유니크한 값 다시 생각해보기.
+                            <div key={title} className={'article'}>
+                                <div className="content">
+                                    <div className="title">
+                                        {title}
+                                    </div>
+                                    <div className="description">
+                                        {description}
+                                    </div>
+                                </div>
+                                <img className="thumbnail" src={thumbnail} />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <style jsx>
+                {`
+                    .article{
+                        width: 100%;
+                        height: 100px;
+                        display: flex;
+                        border-bottom: 1px solid ${EColor.BLACK};
+                    }
+
+                    .article > .thumbnail {
+                        height: 70%;
+                        width: auto;
+                    }
+                `}
+            </style>
+        </>
+    )
+}
--- src/pages/blog/main/[name].tsx
+++ src/pages/blog/main/[name].tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { GetServerSideProps } from "next";
+import { PreviewTable } from "@components/common/article/preview";
 
-interface BlogMainProps {
+export interface BlogMainProps {
     articles: {
         title: string,
+        description: string,
         thumbnail: string,
         uploadDate: string,
     }[]
 }
 
 export default function BlogMain(props: BlogMainProps) {
-    console.log(props);
+    const { 
+        articles,
+    } = props
     return (
         <>
-            목록들
+            <PreviewTable articles={articles}/>
         </>
     )
 };
@@ -26,16 +30,19 @@ export const getServerSideProps: GetServerSideProps = async(context) => {
     const articles = [
         {
             title: '1번 게시글',
+            description: '햄스터 사진입니다.',
             thumbnail: 'https://t1.daumcdn.net/cfile/tistory/167B1F4050FE0C040A',
             uploadDate: '어제',
         },
         {
             title: '2번 게시글',
+            description: '햄스터 사진입니다.',
             thumbnail: 'https://tse1.mm.bing.net/th/id/OIP.Nl8pmiSYLYJsq1_FRW1VDwHaE8?pid=ImgDet&w=1050&h=700&rs=1',
             uploadDate: '어제',
         },
         {
             title: '3번 게시글',
+            description: '햄스터 사진입니다.',
             thumbnail: 'https://tse3.mm.bing.net/th/id/OIP.gDMqmrUfK3qf2OyJJqtj6AHaE8?pid=ImgDet&w=1050&h=700&rs=1',
             uploadDate: '어제',
         },
